e033f771cd26524cf713e8c2b8e36888f0ef0c69
Herman <smurovnemez@mail.ru>
2022-10-27 09:06:08 -0500

    feat: create view components & router setting vue router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,16 +1,48 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from '@/views/HomeView.vue';
+import RegisterView from '@/views/RegisterView.vue';
+import LoginView from '@/views/LoginView.vue';
+import VerifyEmailView from '@/views/VerifyEmailView.vue';
+import ProfileViewVue from '@/views/ProfileView.vue';
+
+const routes = [
+  {
+    name: 'home',
+    path: '/',
+    component: HomeView,
+  },
+  {
+    name: 'register',
+    path: '/register',
+    component: RegisterView,
+  },
+  {
+    name: 'login',
+    path: '/login',
+    component: LoginView,
+  },
+  {
+    name: 'verifyemail',
+    path: '/verifyemail',
+    component: VerifyEmailView,
+    children: [
+      {
+        name: 'verifyemail',
+        path: ':verificationCode',
+        component: VerifyEmailView,
+      },
+    ],
+  },
+  {
+    name: 'profile',
+    path: '/profile',
+    component: ProfileViewVue,
+  },
+];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-   
-  ]
-})
+  history: createWebHistory('/'),
+  routes,
+});
 
-export default router
+export default router;
